45e98bd4c7f0f52fb42228260b7e4c2f279e963b
Максим Хорло <m.horlo@kbsp.loc>
2021-03-05 11:19:45 +0300

    Добавлено описание интерфейса взаимодействия с QCustomPlot.

--- header/icustomplotbuilder.h
+++ header/icustomplotbuilder.h
@@ -8,6 +8,9 @@
 class ICustomPlotBuilder;
 using CustomPlotBuilderPtr = std::shared_ptr<ICustomPlotBuilder>;
 
+/*!
+ * \brief The ICustomPlotBuilder class Интерфейсный класс для взаимодей с QCustomPlot
+ */
 class ICustomPlotBuilder : public IBaseGraphBuilder
 {
     protected:
@@ -15,17 +18,59 @@ class ICustomPlotBuilder : public IBaseGraphBuilder
 
     public:
         virtual QWidget* widget() = 0;
+
+        /*!
+         * \brief showViewer Отображение виджета для вывода графиков
+         */
         virtual void showViewer() = 0;
+        /*!
+         * \brief clearViewer Очистка вижжета от графиков
+         */
         virtual void clearViewer() = 0;
+        /*!
+         * \brief addValues Добавить данные
+         * \param values
+         */
         virtual void addValues(const QVector<QPair<double, double>> &values) = 0;
+        /*!
+         * \brief addValues Добавить данные
+         * \param values
+         */
         virtual void addValues(const std::vector<std::pair<double, double>> &values) = 0;
+        /*!
+         * \brief addGraph Добавить график
+         * \param values Данные для графика
+         * \param nameGraph Название графика, отображается в легенде
+         */
         virtual void addGraph(const QVector<QPair<double, double>> &values, const QString &nameGraph = QString()) = 0;
 //        virtual void addGraph(const std::vector<std::pair<double, double>> &values, const QString &nameGraph = QString()) = 0;
+        /*!
+         * \brief xAxisMinChanged
+         * \param value
+         */
         virtual void xAxisMinChanged(double value) = 0;
+        /*!
+         * \brief xAxisMaxChanged
+         * \param value
+         */
         virtual void xAxisMaxChanged(double value) = 0;
+        /*!
+         * \brief yAxisMinChanged
+         * \param value
+         */
         virtual void yAxisMinChanged(double value) = 0;
+        /*!
+         * \brief yAxisMaxChanged
+         * \param value
+         */
         virtual void yAxisMaxChanged(double value) = 0;
+        /*!
+         * \brief showHideLegend Отобразить/скрыть легенду по графикам
+         */
         virtual void showHideLegend() = 0;
+        /*!
+         * \brief draw Рисует графики по данным, добавленным ранее
+         */
         virtual void draw() = 0;
         static CustomPlotBuilderPtr CreateInstance(); // функция-фабрика
 };
