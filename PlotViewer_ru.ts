c2bdd567e37eb76061c62c9d8b94ef5dd6acd2c0
Максим Хорло <m.horlo@kbsp.loc>
2021-03-13 16:01:35 +0300

    Добавлен файл перевода.

---
+++ PlotViewer_ru.ts
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="ui/mainwindow.ui" line="14"/>
+        <source>Визуализатор графиков</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="77"/>
+        <source>Xmax</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="84"/>
+        <source>Ymax</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="91"/>
+        <source>Легенда</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="98"/>
+        <source>Ymin</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="105"/>
+        <source>Xmin</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="132"/>
+        <source>Файл</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="142"/>
+        <source>Инструменты</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="151"/>
+        <source>Открыть файл...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="156"/>
+        <source>Выход</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="161"/>
+        <source>Очистить</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="166"/>
+        <source>Ввод из строки..</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ParamsDialog</name>
+    <message>
+        <location filename="ui/paramsdialog.ui" line="14"/>
+        <source>Parameters</source>
+        <translation>Параметры</translation>
+    </message>
+    <message>
+        <location filename="ui/paramsdialog.ui" line="23"/>
+        <source>Apply</source>
+        <translation>Применить</translation>
+    </message>
+    <message>
+        <location filename="ui/paramsdialog.ui" line="43"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="src/paramsdialog.cpp" line="30"/>
+        <source>Name</source>
+        <translation>Название</translation>
+    </message>
+    <message>
+        <location filename="src/paramsdialog.cpp" line="30"/>
+        <source>Color</source>
+        <translation>Цвет</translation>
+    </message>
+    <message>
+        <location filename="src/paramsdialog.cpp" line="30"/>
+        <source>Line style</source>
+        <translation>Стиль линии</translation>
+    </message>
+    <message>
+        <location filename="src/paramsdialog.cpp" line="30"/>
+        <source>Point style</source>
+        <translation>Стиль точки</translation>
+    </message>
+</context>
+<context>
+    <name>functionStringDialog</name>
+    <message>
+        <location filename="ui/functionstringdialog.ui" line="32"/>
+        <source>Ввод функции</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
